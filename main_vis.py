1ce15915161cffbf91b81e51f5dc98f31c54a3e4
tinewildiers <62386767+tinewildiers@users.noreply.github.com>
2023-03-06 09:55:35 +0100

    added apple exercise files

--- main_vis.py
+++ main_vis.py
@@ -8,10 +8,10 @@ import smoothness
 from Visualizer import Visualizer
 
 if __name__ == '__main__':
-    visualizer = Visualizer("data\QuestController_still.csv")
+    visualizer = Visualizer("data\QuestController_60cm.csv")
     #visualizer.applyMovingAvg()
     visualizer.visualize3D()
-    visualizer.visualizeHand(False) # True als ge rechts wilt visualizeren
+    visualizer.visualizeHand(True) # True als ge rechts wilt visualizeren
     plt.show()
     visualizer.fft(visualizer.time_vector,visualizer.speed_vector)
     plt.show()
